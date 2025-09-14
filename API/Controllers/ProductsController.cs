b3d3d864548650a095ac6092b4d35e633a44879d
robertgruevski <robertgruevski@outlook.com>
2025-09-14 15:51:45 -0400

    Adding pagination part 1

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,9 +1,7 @@
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
