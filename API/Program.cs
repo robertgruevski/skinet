26fe2863099261e8e480bab1439bab08fba130f2
robertgruevski <robertgruevski@outlook.com>
2025-09-14 17:56:43 -0400

    Adding CORS support on the API

--- API/Program.cs
+++ API/Program.cs
@@ -13,11 +13,16 @@ builder.Services.AddDbContext<StoreContext>(options =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(configuration => configuration.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 try
