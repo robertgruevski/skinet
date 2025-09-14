78417ee193dbbe316fce753342a61d8dbddcbaef
robertgruevski <robertgruevski@outlook.com>
2025-09-13 22:02:16 -0400

    Updating .gitignore

--- Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a651572a50e470c228be9d7fd7a263da3597126")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f86a7d4cde22dbce3764335f47b9ee6d8b1da733")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
