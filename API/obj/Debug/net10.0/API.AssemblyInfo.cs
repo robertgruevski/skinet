2ab03a063cbfc8319ea9009357aa43afa6b5059c
robertgruevski <robertgruevski@outlook.com>
2025-09-13 21:40:30 -0400

    Creating a products controller

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc3709172ed1bedf2895fce79b4e0941fdd9d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd966fb59d5556e791f30cd9a24f3558f1f9541")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
