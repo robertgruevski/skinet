8ae1d84415df7c273e615b63de5bf356cebdd612
robertgruevski <robertgruevski@outlook.com>
2025-09-15 14:28:53 -0400

    Adding the filtering functionality using Material Dialog part 2

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -2,17 +2,24 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/Product';
 import { ShopService } from '../../core/services/shop.service';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-shop',
-  imports: [ProductItemComponent],
+  imports: [ProductItemComponent, MatButton, MatIcon],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent implements OnInit {
   private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
 
   products: Product[] = [];
+  selectedBrands: string[] = [];
+  selectedTypes: string[] = [];
 
   ngOnInit(): void {
     this.initializeShop();
@@ -25,6 +32,26 @@ export class ShopComponent implements OnInit {
       next: response => this.products = response.data,
       error: error => console.log(error)
     });
+  }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px',
+      data: {
+        selectedBrands: this.selectedBrands,
+        selectedTypes: this.selectedTypes
+      }
+    });
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if (result) {
+          console.log(result);
+          this.selectedBrands = result.selectedBrands;
+          this.selectedTypes = result.selectedTypes;
+          // apply filters
+        }
+      }
+    })
   }
 
 }
