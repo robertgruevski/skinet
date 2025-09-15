bd96b0dcb3f44fe6c10f6fea3c44d1e76dfe8202
robertgruevski <robertgruevski@outlook.com>
2025-09-15 16:30:57 -0400

    Designing the product details page

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -2,10 +2,16 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../../core/services/shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../shared/models/Product';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatDivider } from '@angular/material/divider';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CurrencyPipe, MatButton, MatIcon, MatInput, MatFormField, MatLabel, MatDivider],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })
@@ -23,8 +29,8 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (!id) return;
     this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
-      error: error => console.log(error)
-    })
+      next: (product) => (this.product = product),
+      error: (error) => console.log(error),
+    });
   }
 }
