d65d5a57cec405eb54cf601850cb57e18dfab239
robertgruevski <robertgruevski@outlook.com>
2025-09-16 15:31:35 -0400

    Testing the authentication

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using API.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -34,6 +36,14 @@ public class BuggyController : BaseApiController
     {
         return Ok();
     }
-    
-    
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok($"Hello {name} with the id of {id}");
+    }
 }
