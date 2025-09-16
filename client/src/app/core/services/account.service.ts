a8be7352d62301e945732c7c803c82c4da2d4953
robertgruevski <robertgruevski@outlook.com>
2025-09-16 17:48:00 -0400

    Updating the header component

--- client/src/app/core/services/account.service.ts
+++ client/src/app/core/services/account.service.ts
@@ -16,7 +16,7 @@ export class AccountService {
   login(values: any) {
     let params = new HttpParams();
     params = params.append('useCookies', true);
-    return this.http.post<User>(this.baseUrl + 'login', values, { params });
+    return this.http.post<User>(this.baseUrl + 'login', values, { params, withCredentials: true });
   }
 
   register(values: any) {
@@ -24,13 +24,15 @@ export class AccountService {
   }
 
   getUserInfo() {
-    return this.http.get<User>(this.baseUrl + 'account/user-info').subscribe({
-      next: (user) => this.currentUser.set(user),
-    });
+    return this.http
+      .get<User>(this.baseUrl + 'account/user-info', { withCredentials: true })
+      .subscribe({
+        next: (user) => this.currentUser.set(user),
+      });
   }
 
   logout() {
-    this.http.post(this.baseUrl + 'account/logout', {});
+    return this.http.post(this.baseUrl + 'account/logout', {}, { withCredentials: true });
   }
 
   updateAddress(address: Address) {
