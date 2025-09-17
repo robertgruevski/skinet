f899248f78cee0010f62cd9b9e0afa7f9ac7acdd
robertgruevski <robertgruevski@outlook.com>
2025-09-17 16:17:29 -0400

    Validating step completion part 1

--- client/src/app/features/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/features/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
 import { CheckoutService } from '../../../core/services/checkout.service';
 import { MatRadioModule } from '@angular/material/radio';
 import { CurrencyPipe } from '@angular/common';
@@ -14,6 +14,7 @@ import { DeliveryMethod } from '../../../shared/models/DeliveryMethod';
 export class CheckoutDeliveryComponent implements OnInit {
   checkoutService = inject(CheckoutService);
   cartService = inject(CartService);
+  deliveryComplete = output<boolean>();
 
   ngOnInit(): void {
     this.checkoutService.getDeliveryMethods().subscribe({
@@ -22,6 +23,7 @@ export class CheckoutDeliveryComponent implements OnInit {
           const method = methods.find((x) => x.id === this.cartService.cart()?.deliveryMethodId);
           if (method) {
             this.cartService.selectedDelivery.set(method);
+            this.deliveryComplete.emit(true);
           }
         }
       },
@@ -35,6 +37,7 @@ export class CheckoutDeliveryComponent implements OnInit {
     if (cart) {
       cart.deliveryMethodId = method.id;
       this.cartService.setCart(cart);
+      this.deliveryComplete.emit(true);
     }
   }
 }
