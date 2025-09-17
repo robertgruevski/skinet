bc441696a8e5210c0c194171c45e005e4ee5292a
robertgruevski <robertgruevski@outlook.com>
2025-09-17 14:26:31 -0400

    Adding client side Stripe

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey:
+    'pk_test_51S8OfjAarKDYG3gaY1RM3IPY6xo9RaFLGRTqDJwEtgcr8wDuGLgwpnSZBOO33I62tQBDBaVgKOQD0DzIartXHVQJ003AWZ5xN4',
 };
