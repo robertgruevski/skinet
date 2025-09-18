7f33ac764c04e6526e5dddfcd50bbc6e66af20ab
robertgruevski <robertgruevski@outlook.com>
2025-09-18 17:19:31 -0400

    Adding SignalR to the client

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notifications',
   stripePublicKey:
     'pk_test_51S8OfjAarKDYG3gaY1RM3IPY6xo9RaFLGRTqDJwEtgcr8wDuGLgwpnSZBOO33I62tQBDBaVgKOQD0DzIartXHVQJ003AWZ5xN4',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  hubUrl: 'hub/notifications',
   stripePublicKey:
     'pk_test_51S8OfjAarKDYG3gaY1RM3IPY6xo9RaFLGRTqDJwEtgcr8wDuGLgwpnSZBOO33I62tQBDBaVgKOQD0DzIartXHVQJ003AWZ5xN4',
 };
