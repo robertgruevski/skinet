c05451b7c0720a885f1d7dba7fc2785f786d13d2
robertgruevski <robertgruevski@outlook.com>
2025-09-19 18:27:49 -0400

    Removing unnecessary code

--- client/src/app/shared/components/empty-state/empty-state.component.ts
+++ client/src/app/shared/components/empty-state/empty-state.component.ts
@@ -1,12 +1,11 @@
 import { Component, inject, input, output } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
 import { BusyService } from '../../../core/services/busy.service';
 
 @Component({
   selector: 'app-empty-state',
-  imports: [MatIcon, MatButton, RouterLink],
+  imports: [MatIcon, MatButton],
   templateUrl: './empty-state.component.html',
   styleUrl: './empty-state.component.scss',
 })
