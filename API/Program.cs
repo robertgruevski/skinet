04ac150334a6080cb73d46960a3f4064ce31e41c
robertgruevski <robertgruevski@outlook.com>
2025-09-18 20:52:13 -0400

    Removed uneeded code. Mapped fallback controller.

--- API/Program.cs
+++ API/Program.cs
@@ -42,9 +42,13 @@ app.UseCors(configuration => configuration.AllowAnyHeader().AllowAnyMethod().All
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<AppUser>();
 app.MapHub<NotificationHub>("/hub/notifications");
+app.MapFallbackToController("Index", "Fallback");
 
 try
 {
