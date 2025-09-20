c3dca0d7aab8aee588995e91082d1661d8f7061a
robertgruevski <robertgruevski@outlook.com>
2025-09-19 21:18:22 -0400

    Adding refund functionality

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [Authorize(Roles = "Admin")]
-public class AdminController(IUnitOfWork unit) : BaseApiController
+public class AdminController(IUnitOfWork unit, IPaymentService paymentService) : BaseApiController
 {
     [HttpGet("orders")]
     public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrders([FromQuery] OrderSpecParams specParams)
@@ -29,5 +29,30 @@ public class AdminController(IUnitOfWork unit) : BaseApiController
         if (order is null) return BadRequest("No order with that id");
 
         return order.ToDto();
+    }
+
+    [HttpPost("orders/refund/{id:int}")]
+    public async Task<ActionResult<OrderDto>> RefundOrder(int id)
+    {
+        var spec = new OrderSpecification(id);
+
+        var order = await unit.Repository<Order>().GetEntityWithSpec(spec);
+
+        if (order is null) return BadRequest("No order with that id");
+
+        if (order.Status == OrderStatus.Pending) return BadRequest("Payment not received for this order");
+
+        var result = await paymentService.RefundPayment(order.PaymentIntentId);
+
+        if (result == "succeeded")
+        {
+            order.Status = OrderStatus.Refunded;
+
+            await unit.Complete();
+
+            return order.ToDto();
+        }
+
+        return BadRequest("Problem refunding order");
     }
 }
